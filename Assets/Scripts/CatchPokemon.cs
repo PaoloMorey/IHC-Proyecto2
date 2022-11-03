b97ad3d7f3af23b4546b5010b0bd4d716def9b69
DiegoOrtizS <diego.ortiz@utec.edu.pe>
2022-11-03 16:17:31 -0500

    feature: particles

--- Assets/Scripts/CatchPokemon.cs
+++ Assets/Scripts/CatchPokemon.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class CatchPokemon : MonoBehaviour
 {
+    public GameObject catchFx; // particle
+    private Animator anim;
+    private GameObject pokemon;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        pokemon = null;
+    }
+
     float GetCatchFormula(int catchRate)
     {
         return catchRate*GameVariables.maxPercentCatchRate/GameVariables.maxCatchRate;
@@ -12,15 +22,36 @@ public class CatchPokemon : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("Pokemon")) {
-            GameObject pokemon = collision.gameObject;
+            pokemon = collision.gameObject;
             if (GetCatchFormula(pokemon.GetComponent<PokemonData>().GetCatchRate()) <= Random.Range(0.0f, 1.0f)) {
                 Debug.Log("Catching "+name);
-                Destroy(gameObject);
-                Destroy(pokemon);
+                anim.SetInteger("Capture", 1);
+                // Instantiate(catchFx, transform.position, transform.rotation);
+                // Destroy(catchFx, Random.Range(0.5f, 1.0f));
+                // Destroy(gameObject);
+                // Destroy(pokemon);
             }
             else {
-                Destroy(gameObject);
+                pokemon.SetActive(false);
+                anim.SetInteger("Capture", -1);
             }
+        }
+    }
+
+    public void FinishEvent()
+    {
+        if (anim.GetInteger("Capture") == -1)
+        {
+            Destroy(gameObject);
+            if (pokemon != null)
+                pokemon.SetActive(true);
+        }
+        else if (anim.GetInteger("Capture") == 1)
+        {
+            Instantiate(catchFx, transform.position, transform.rotation);
+            Destroy(catchFx, Random.Range(0.5f, 1.0f));
+            Destroy(gameObject);
+            Destroy(pokemon);
         }
     }
 }
