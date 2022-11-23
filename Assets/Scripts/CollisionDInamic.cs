3e995fe3eeb29edb88dcdfe6443504abbc1a96ff
unknown <mateo.noel@utec.edu.pe>
2022-11-23 13:41:19 -0500

    nuevos pokemones

---
+++ Assets/Scripts/CollisionDInamic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDInamic : MonoBehaviour
+{
+  SphereCollider colider;
+    // Start is called before the first frame update
+    void Start()
+    {
+      colider = GetComponent<SphereCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //colider.center = 
+    }
+}
