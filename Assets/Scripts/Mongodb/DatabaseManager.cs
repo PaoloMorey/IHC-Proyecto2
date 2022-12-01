2ebe60763d936621a5d01c06220bb3987efc5125
DiegoOrtizS <diego.ortiz@utec.edu.pe>
2022-11-30 21:18:24 -0500

    probando

--- Assets/Scripts/Mongodb/DatabaseManager.cs
+++ Assets/Scripts/Mongodb/DatabaseManager.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using MongoDB.Driver;
 
+// public class MongoClientSettings : IEquatable<MongoClientSettings> {}
+
 public class DatabaseManager : MonoBehaviour
 {
-    private const string MONGO_URI = "mongodb+srv://admin:uiXQhRYHEpbXQO0o@pokemonvr.wxfm8vr.mongodb.net/test";
+    // private const string MONGO_URI = "mongodb+srv://admin:uiXQhRYHEpbXQO0o@pokemonvr.wxfm8vr.mongodb.net/test";
+    // private const string MONGO_URI = "mongodb//admin:uiXQhRYHEpbXQO0o@pokemonvr.wxfm8vr.mongodb.net/?retryWrites=true&w=majority";
+    private const string MONGO_URI = "mongodb://admin:uiXQhRYHEpbXQO0o@ac-lxjzoz7-shard-00-00.wxfm8vr.mongodb.net:27017,ac-lxjzoz7-shard-00-01.wxfm8vr.mongodb.net:27017,ac-lxjzoz7-shard-00-02.wxfm8vr.mongodb.net:27017/?ssl=true&replicaSet=atlas-d0wuk1-shard-0&authSource=admin&retryWrites=true&w=majority";
     private const string DATABASE_NAME = "PokemonVR_DB";
     private MongoClient client;
     private IMongoDatabase db;
@@ -14,7 +18,9 @@ public class DatabaseManager : MonoBehaviour
 
     void Awake()
     {
-        client = new MongoClient(MONGO_URI);
+        MongoClientSettings settings = MongoClientSettings.FromUrl(MongoUrl.Create(MONGO_URI));
+        // settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+        client = new MongoClient(settings);
         db = client.GetDatabase(DATABASE_NAME);
         pokemonCollection = db.GetCollection<ModelPokemon>("Pokemon");
         playerCollection = db.GetCollection<ModelPlayer>("Player");
